07f08e08e11daf00f8fd5523528eb8d6dce2b733
Shaim Khanusiya <shaim456@gmail.com>
2024-02-08 22:10:47 +0530

    add cors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import vehicleRoutes from "./routes/vehicleRoutes.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use(express.json());
 
